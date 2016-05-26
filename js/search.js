a1d4896dca5d2d13aa766c5b43dcb872b7fac7f8
Vassell, Ashley <ashley.vassell@ge.com>
2016-05-26 10:52:03 -0400

    ashleys updates
    validation, widgets, mob ops, moved all css in html to the style.css

--- js/search.js
+++ js/search.js
@@ -2,14 +2,24 @@
 JavaScript for index.html, contact.js
 */
 
-function searchTerms(zip,distance) {
+function searchTerms(zip, distance) {
+		
+		  var numbers = /^[0-9]+$/;  
           var zipCode = zip;
           var distance = distance;
-          if (zip!=""){
-          localStorage.setItem('zipCode', zipCode);
-          } 
-          if (distance !="") {
-          localStorage.setItem('distance', distance);
-          }
-          window.location.href = 'map.html';
-      }
+		  
+		  if(!zipCode.match(numbers) || zipCode.length !=5){
+			alert('Please use 5 numbers for the Zip Code')
+			return false;
+		  }
+		  
+		  else{
+			  if (zip!=""){
+			  localStorage.setItem('zipCode', zipCode);
+			  } 
+			  if (distance !="") {
+			  localStorage.setItem('distance', distance);
+			  }
+			  window.location.href = 'map.html';
+		  }	
+}
